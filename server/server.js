c877e6535b2cb6f17e4b2bb0fa52c3cbe58f808f
Sumiyah <sumiyah.fa@gmail.com>
2021-01-27 15:52:03 +0300

    changes in server.js

--- server/server.js
+++ server/server.js
@@ -4,15 +4,20 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 8000
+const path = require('path')
 
 require('./config/mongoose.config')
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 require('./routes/authors.routes')(app)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.resolve('../client/build')));
 }
 
-app.listen(port, () => console.log(`Listening on port ${port}`) )
+app.all('*', (req, res, next) => {
+    res.sendFile(path.resolve('../client/build/index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`))

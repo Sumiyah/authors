7ede5dbca00f01ed4f3f1c7061c37cd0435238a8
Sumiyah <sumiyah.fa@gmail.com>
2021-01-27 16:24:47 +0300

    more changes

--- server/server.js
+++ server/server.js
@@ -10,14 +10,16 @@ require('./config/mongoose.config')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+ //if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 require('./routes/authors.routes')(app)
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
 
-app.all('*', (req, res, next) => {
-    res.sendFile(path.resolve('../client/build/index.html'));
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, "../client", 'build', 'index.html');
+    console.log(index)
+    res.sendFile(index);
   });
 
 app.listen(port, () => console.log(`Listening on port ${port}`) )

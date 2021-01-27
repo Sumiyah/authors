e9ab2495075b094266d951d03622689fd07815ba
Sumiyah <sumiyah.fa@gmail.com>
2021-01-27 11:00:21 +0300

    more tries and changes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ console.log("In server.js")
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 
 require('./config/mongoose.config')
 app.use(cors())
@@ -11,6 +11,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 require('./routes/authors.routes')(app)
 
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`) )

bec019ad87744d76d193f25773e277fbfbf1bc92
Sumiyah <sumiyah.fa@gmail.com>
2021-01-27 15:26:39 +0300

    ?

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,16 @@ import { Router } from '@reach/router';
 import AuthorsList from './Components/AuthorsList';
 import AuthorsForm from './Components/AuthorForm';
 import UpdateAuthor from './Components/UpdateAuthor';
+import Test from './Components/Test';
 
 function App() {
   return (
     <div className="container mt-5">
-      <Router>
+      <Router primary={false}>
         <AuthorsList path="/" />
         <AuthorsForm path="/new" />
         <UpdateAuthor path='/view/:_id' />
+        <Test path="/test" />
       </Router>
     </div>
   );
---
+++ client/src/Components/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+    return (
+        <div>
+            <h2>Testing this page</h2>
+        </div>
+    )
+}
+
+export default Test
